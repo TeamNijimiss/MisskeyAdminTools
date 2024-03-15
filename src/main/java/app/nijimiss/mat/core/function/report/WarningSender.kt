57b4a7152df06a8be73981458a4e81224e1bc2e7
nafu-at <satsuki@nafusoft.dev>
2024-03-15 21:32:28 +0900

    Update copyright year.

--- src/main/java/app/nijimiss/mat/core/function/report/WarningSender.kt
+++ src/main/java/app/nijimiss/mat/core/function/report/WarningSender.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Nafu Satsuki
+ * Copyright 2024 Nafu Satsuki
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
